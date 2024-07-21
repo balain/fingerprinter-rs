24c02122bdbe55942ecce67d64aa8ffd837b645d
John <balain@users.noreply.github.com>
2024-07-21 16:05:04 -0400

    Working after removing jwalk

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,76 @@
+/*
+ * Process the provided folder and calculate SHA256 hashes for all files not in an exclude list
+ *
+ * Exclude list can match any of the directory hierarchy (but must fully match)
+ *
+ * Originally based on code from: https://stackoverflow.com/a/63543625
+ */
+
 use std::time::{Duration, Instant};
-use std::path::PathBuf;
-use jwalk::Parallelism;
-use jwalk::rayon::iter::ParallelIterator;
-use jwalk::rayon::iter::ParallelBridge;
+use std::path::{Path, PathBuf};
+use std::fs::*;
+use std::io;
+use sha2::{Sha256, Digest};
+use clap::Parser;
 
-fn include_file<'a>(p: &PathBuf, exclude: &Vec<&str>, md5st: Vec<&'a str>) -> (bool, Vec<&'a str>) {
-    let components: Vec<_> = p.components().map(|comp| comp.as_os_str()).collect();
-    // Option A: Process if the first path element is NOT in exclude[]
-    // if !exclude.contains(&components[1].to_str().unwrap()) {
-    //     //println!("{:?}", p.as_os_str());
-    // }
-
-    // Option B: Process if NO exclude[] element is anywhere in the path
-    // N.B. count > 0 means there's at least one match between them
-    // https://stackoverflow.com/a/29504547
-
-    let match_count= components.iter().filter(|&x| exclude.contains(&x.to_str().unwrap())).count();
-    if match_count == 0 {
-        // No matches, so process
-        // println!("Processing {:?}", p.as_os_str());
-        (true, md5st)
-    } else {
-        // println!("Skipping {:?}", p.as_os_str());
-        (false, md5st)
-    }
+/// Simple path processor
+#[derive(Parser, Debug)]
+#[command(version, about, name = ".")]
+struct Args {
+    /// Path to process
+    #[arg(short, long)]
+    pathname: String,
 }
 
-fn go<'a>(exclude:Vec<&'a str>,  md5st: &mut Vec<&'a str>) -> (Vec<&'a str>, usize) {
-    let ctr:usize;
-    ctr = jwalk::WalkDir::new(".")
-        .parallelism(Parallelism::RayonNewPool(0))
-        .into_iter()
-        .par_bridge()
-        .filter_map(|dir_entry_result| {
-            let dir_entry = dir_entry_result.ok()?;
-            if dir_entry.file_type().is_file() {
-                let path: PathBuf = dir_entry.path();
-                let infile: bool;
-                (infile, *md5st) =  include_file(&path, &exclude, md5st.clone()); // Error here
-                if infile {
-                    println!("Calculating md5 for {:?}", path.as_os_str());
-                    let fname: &str = &path.to_str().unwrap_or("undef_result_a"); // Error here
-                    md5st.push(fname.clone()); // Error here
-                    return Some(true)
-                } else {
-                    // println!("Skipping file: {:?}", path.as_os_str());
-                    return Some(false)
-                }
+// Originally based on https://stackoverflow.com/questions/63542762
+// The Cookbook has some good suggestions: https://rust-lang-nursery.github.io/rust-cookbook/file/dir.html
+// Tried jwalk but had issues with borrowing, so went back to a simpler implementation
+fn visit(path: &Path, cb: &mut dyn FnMut(PathBuf), exclude:Vec<&str>) -> io::Result<()> {
+    for e in read_dir(path)? {
+        let e = e?;
+        let path = e.path();
+
+        // Hash processing based on https://github.com/RustCrypto/hashes example "Hashing Readable Objects"
+        let components: Vec<_> = path.components().map(|comp| comp.as_os_str()).collect();
+        // Only include files that don't have a component in the exclude vector
+        // logic from: https://stackoverflow.com/a/29504547
+        let match_count= components.iter().filter(|&x| exclude.contains(&x.to_str().unwrap())).count();
+        if match_count == 0 {
+            if path.is_dir() {
+                visit(&path, cb, exclude.clone())?;
+            } else if path.is_file() {
+                cb(path);
             }
-            None
-        }).count();
-    (md5st.clone(), ctr)
+        }
+    }
+
+    Ok(())
 }
 
 fn main() {
-    let mut md5st:Vec<&str> = Vec::new();
-    // MD5ST.clone().push(  String::as_mut_str( &mut String::from("asdf")));
-    let start: Instant = Instant::now();
+    let start: Instant = Instant::now(); // Start timer
 
-    let exclude_list:Vec<&str> = vec!["src", "raw", "lib", "target", "node_modules"];
-    // let first_entry = String::from("asdf");
-    // md5st.push(&*first_entry);
+    // Parse cli arguments
+    let args = Args::parse();
+    let pathname:String = args.pathname;
 
-    let result_count:usize;
-    (md5st, result_count) = go(exclude_list, &mut md5st.to_owned());
-    println!("Count: {:?}", result_count);
-    println!("md5 vec len = {:?}",  md5st.len());
+    // List of paths (full string) to exclude
+    // If any one of these items is found in the full path, that entry will be ignored/excluded
+    let exclude = vec![".idea", "target", ".git", "node_modules", "lib"];
 
-    let dur: Duration = start.elapsed();
-    println!("Time elapsed: {:?}", dur);
+    let path = Path::new(pathname.as_str());
+    let mut files = Vec::new();
+    visit(path, &mut |e| files.push(e), exclude).unwrap();
+    // TODO: Add Rayon to the iterator loop
+    for f in files {
+        // https://github.com/RustCrypto/hashes/tree/master/sha2
+        let mut file = File::open(f.clone()).unwrap();
+        let mut hasher = Sha256::new();
+        let _n = io::copy(&mut file, &mut hasher);
+        let hash = hasher.finalize();
+        println!("{:?}: {:x}", f, hash); // TODO: Change to JSON output
+    }
+
+    let dur: Duration = start.elapsed();  // End timer
+    eprintln!("Time elapsed: {:?}", dur); // Show elapsed time to STDERR
 }
